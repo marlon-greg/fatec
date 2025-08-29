f532848a806002f792a6a69633484bda3f2262ab
marlon-greg <marlonfanger@gmail.com>
2025-08-29 19:26:11 -0300

    ex1-aula4

---
+++ programacao_orientada_objeto/src/semana04/Excecao01.java
@@ -0,0 +1,10 @@
+package semana04;
+
+import javax.swing.JOptionPane;
+
+public class Excecao01{
+    public static void main(String[] args) {
+        
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Idade? "));
+    }
+}
