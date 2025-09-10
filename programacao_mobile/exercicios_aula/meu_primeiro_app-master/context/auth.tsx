27f6749f6c2bb432e1e70538285b0588f79c1f13
marlon-greg <marlonfanger@gmail.com>
2025-09-10 20:03:07 -0300

    login basico com firebase

--- programacao_mobile/exercicios_aula/meu_primeiro_app-master/context/auth.tsx
+++ programacao_mobile/exercicios_aula/meu_primeiro_app-master/context/auth.tsx
@@ -1,5 +1,8 @@
 import React, { createContext, useContext, useState } from 'react'
-import {router} from 'expo-router'
+import { router } from 'expo-router'
+import {app} from '../services/firebase'
+import {initializeAuth, signInWithEmailAndPassword} from 'firebase/auth'
+
 
 interface IUser{
     email: string
@@ -18,15 +21,25 @@ interface IAuthProviderProps{
 
 const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 
-export const AuthProvider: React.FC<IAuthProviderProps> = ({children}) => {
+export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }: IAuthProviderProps) => {
     const [user, setUser] = useState<IUser>({email: "", password: ""})
 
     function handleLogin(){
-        if(!user || user.email != "admin" || user.password != "admin"){
-            alert("Erro ao realizar o login")
+        if(!user.email || !user.password){
+            alert("Digite o email e a senha")
             return
+
         }
-        router.push('home')
+
+        const auth = initializeAuth(app)
+        signInWithEmailAndPassword(auth, user.email, user.password)
+        .then(() => {
+            router.push('home')
+        }
+        ).catch((error) => {
+           console.log('falha ao autenticar')
+           alert("Falha ao autenticar, verifique suas credenciais")
+            })
     }
     return(
         <AuthContext.Provider value={{user, handleLogin, setUser}}>
@@ -38,3 +51,5 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({children}) => {
 export function useAuth(){
     return useContext(AuthContext)
 }
+
+
---
+++ programacao_mobile/exercicios_aula/meu_primeiro_app-master/services/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ0PdQYF0RK8v3gDk7SivpaPjmWLtWGEE",
+  authDomain: "exemplo-app-rcnative-fatec.firebaseapp.com",
+  projectId: "exemplo-app-rcnative-fatec",
+  storageBucket: "exemplo-app-rcnative-fatec.firebasestorage.app",
+  messagingSenderId: "1057891580810",
+  appId: "1:1057891580810:web:8d428fae539ecbb89f6673",
+  measurementId: "G-RW4ZJQTSM2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
